0dc3a51046f87a3f032b72a9e6e5b3649c087a7a
Derek Hogue <derek@amphibian.info>
2015-10-28 10:57:11 -0500

    Ahem

--- ee3/user/addons/tally/addon.setup.php
+++ ee3/user/addons/tally/addon.setup.php
@@ -3,7 +3,7 @@
 return array(
       'author' => 'Amphibian',
       'author_url' => 'http://amphibian.info',
-      'description' => 'Tally or average numbers in an entries loop..',
+      'description' => 'Tally or average numbers in an entries loop.',
       'docs_url' => 'https://github.com/amphibian/tally.ee_addon',
       'name' => 'Tally',
       'namespace' => 'Amphibian\Tally',
